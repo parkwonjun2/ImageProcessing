9deff10f0a33cdbde48de645967f4393a8592829
JJoni <pwj970314@naver.com>
2023-03-15 13:32:55 +0900

    Master 2

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ dst1, dst2, dst3 = None, None, None # 영상처리한 결과
 src = cv2.imread('c:/images/picture24.jpg') # 이미지 읽기
 
 dat1 = grayScale(scr)
+dat2 = cannyEdge(scr)
 
 
 
 cv2.imshow('Src', src) # 화면 출력
 cv2.imshow('Dts1', dts1) # 화면 출력
+cv2.imshow('Dts2', dts2) # 화면 출력
 
 ## 마무리
 cv2.waitkey(0)
